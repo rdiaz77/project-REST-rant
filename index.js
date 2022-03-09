0bb9c6329d93a5113eb7e04f1d7b90d0ebd89575
RDM_ <rafadiazm77@gmail.com>
2022-03-09 12:13:18 -0600

    404 status added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req,res) =>{
-    res.send('hello world rant')
+    res.status(404).send(`
+    <h1>404 page</h1>`)
 
 })
 
