0b51fcc11dcbff26c27b2ba95ca3f9dbff9b3b3b
RDM_ <rafadiazm77@gmail.com>
2022-03-10 17:59:37 -0600

    error404 page

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/', (req,res) => {
 })
 
 app.get('*', (req,res) =>{
-    res.status(404).send(`
-    <h1>404 page</h1>`)
+    
+    res.render('error404')
 
 })
 
---
+++ views/error404.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404(){
+    return(
+    <def>
+        <main>
+            <h1>404: PAGE NOT FoUND</h1>
+            <p>Oops, sorry, we can't find this page!</p>
+        </main>
+
+    </def>
+
+
+    )
+    
+}
+
+module.exports = error404
