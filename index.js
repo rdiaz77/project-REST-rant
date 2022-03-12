8df5b12c981c9e13c140cf0cc47b9d7a9549cb0b
RDM_ <rafadiazm77@gmail.com>
2022-03-12 11:19:12 -0600

    adding css style

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const router = require('./controller/places');
 const app = express()
 
+//Express settings
+
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Controllers and routes
 app.use('/places', require('./controller/places'))
 
 
