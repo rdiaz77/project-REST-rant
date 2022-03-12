ebee39ac220389a40d33d2932b9d1eb805824bc4
RDM_ <rafadiazm77@gmail.com>
2022-03-11 19:03:42 -0600

    end

--- controller/places.js
+++ controller/places.js
@@ -4,7 +4,20 @@ const router = require('express').Router()
 // GET /places
 
 router.get('/', (req,res) => {
-    res.render('places/index')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+    res.render('places/index', {places})
 })
 
 
