973bd61fee14dd49e374ff88a07f19fadcf75046
RDM_ <rafadiazm77@gmail.com>
2022-03-14 20:05:24 -0500

    show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,10 +2,14 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
       return (
         <div className = 'col-sm-6'>
-          <h2>{place.name}</h2>
+          <h2>
+            <a href = {`/places/${index}`}>
+            {place.name}
+            </a>
+            </h2>
           <img src={place.pic} alt={place.name}/>
           <div>
             Photo by <a href="https://unsplash.com/@shaianramesht?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">shaian ramesht</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
